19263319b958263e27b8e33b963a68d78121cd43
TurtleZhong <xinliangzhong@foxmail.com>
2016-11-23 20:44:29 +0800

    just do some exercise about python

--- test.py
+++ test.py
@@ -2,8 +2,10 @@
 # Filename:test.py
 
 def sayHello():
-    print 'Hello World!'  # block belonging to the function
+    print '"Hello World!"'  # block belonging to the function
 
+n = input("Input a num")
+print "n =", n
 
 sayHello()  # call the function
 
@@ -60,3 +62,33 @@ def func3(a, b=5, c=10):
 func3(56)
 
 print func3.__doc__
+
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+
+endings = ['st', 'nd', 'rd'] + 17 * ['th'] + \
+          ['st', 'nd', 'rd'] + 7*['th'] + \
+          ['st']
+
+
+year = raw_input('Year: ')
+month = raw_input('Month(1-12): ')
+day = raw_input('Day(1-31): ')
+
+month_number = int(month)
+day_number = int(day)
+
+month_name = months[month_number - 1]
+day_name = day + endings[day_number - 1]
+
+print month_name + ' ' + day_name + ', ' + year
